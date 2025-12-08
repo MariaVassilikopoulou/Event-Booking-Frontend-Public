134aab05f20b1bcda28c920db4b2d5c223278a1f
maria vassilikopoulou <maria.vassilikopoulou@iths.se>
2025-12-08 14:56:14 +0100

    changes

--- src/services/eventService.ts
+++ src/services/eventService.ts
@@ -1,41 +1,21 @@
 import { Event } from "@/types/globalTypes";
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_EVENTS;
-if (!BASE_URL) {
-  throw new Error("Missing NEXT_PUBLIC_API_EVENTS in .env file");
-}
-export const getEvents = async (): Promise<Event[]| null> => {
-  try{
-  
-  const response = await fetch(BASE_URL, {
-    cache: "no-store",
-  });
-  if (!response.ok) {
-    console.error("Failed to fetch events:", response.status);
-    return null;
-  }
 
-  return response.json();
-} catch (error) {
-  console.error("Error fetching in getEvents:", error);
-  return null;
-}
-};
+const BASE_URL =process.env.NEXT_PUBLIC_API_EVENTS;
+ 
 
-export const getEventsById = async (id: string): Promise<Event| null> => {
-  try{
-  const response = await fetch(`${BASE_URL}/${id}`, {
-    cache: "no-store",
-  });
-  if (!response.ok) {
-    const text = await response.text();
-    console.error(`Failed to fetch event ${id}:`, response.status, text);
-    //throw new Error("Failed to fetch event details");
-    return null;
+export const getEvents= async(): Promise<Event[]>=>{
+  if (!BASE_URL) {
+    throw new Error("Missing NEXT_PUBLIC_API_EVENTS in .env file");
   }
-  return response.json();
-  }catch(error){
-    console.error(`Error fetching in getEventsById for id ${id}:`, error);
-    return null;
-  }
-};
+    const res = await fetch (BASE_URL);
+    if (!res.ok) throw new Error("failed to fetch events");
+    return res.json();
+}
+
+
+export const getEventsById= async(id: string):Promise<Event>=>{
+    const res= await fetch (`${BASE_URL}/${id}`);
+    if(!res.ok) throw new Error("Failed to fetch event");
+    return res.json();
+}
