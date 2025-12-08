32c167d01ef11ae12127b0e9a1fcf8103c1d1f64
maria vassilikopoulou <maria.vassilikopoulou@iths.se>
2025-12-08 14:46:25 +0100

    service changes

--- src/services/eventService.ts
+++ src/services/eventService.ts
@@ -1,23 +1,32 @@
 import { Event } from "@/types/globalTypes";
 
-const BASE_URL = "/api/events";
-
+const BASE_URL = process.env.NEXT_PUBLIC_API_EVENTS;
+if (!BASE_URL) {
+  throw new Error("Missing NEXT_PUBLIC_API_EVENTS in .env file");
+}
 export const getEvents = async (): Promise<Event[]| null> => {
   try{
-  const response = await fetch(BASE_URL);
+  
+  const response = await fetch(BASE_URL, {
+    cache: "no-store",
+  });
   if (!response.ok) {
-    throw new Error("Failed to fetch events");
-  }
-  return response.json();}
-  catch(error){
-    console.error("Error fetching in getEvents:", error);
+    console.error("Failed to fetch events:", response.status);
     return null;
   }
+
+  return response.json();
+} catch (error) {
+  console.error("Error fetching in getEvents:", error);
+  return null;
+}
 };
 
 export const getEventsById = async (id: string): Promise<Event| null> => {
   try{
-  const response = await fetch(`${BASE_URL}/${id}`);
+  const response = await fetch(`${BASE_URL}/${id}`, {
+    cache: "no-store",
+  });
   if (!response.ok) {
     const text = await response.text();
     console.error(`Failed to fetch event ${id}:`, response.status, text);
