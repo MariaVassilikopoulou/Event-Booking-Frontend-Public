37907ef32b863963992bbe4f07f9873e8418d07a
maria vassilikopoulou <maria.vassilikopoulou@iths.se>
2025-12-08 14:19:40 +0100

    receive data events correct

--- src/app/events/[id]/page.tsx
+++ src/app/events/[id]/page.tsx
@@ -3,15 +3,18 @@ import { getEventsById } from "@/services/eventService";
 import EventDetailsClient from "../../../components/EventDetailsClient"; 
 import { Event } from "@/types/globalTypes";
 
+interface Params {
+    params: { id: string };
+  }
 
-export default async function EventDetailsPage({ params }: { params:Promise<{ id: string }> }) {
-    
-    const{ id } =await params;
-    let event: Event | null =  await getEventsById(id);
+  export default async function EventDetailsPage({ params }: Params) {
+    const { id } = params;
+    let event: Event | null =  null;
     
     try {
        
         event = await getEventsById(id);
+        if (!event) notFound();
     } catch (error) {
         console.error(`Failed to fetch event ${id}:`, error);
        
